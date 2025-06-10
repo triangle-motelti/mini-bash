34d456dc6b5bc2d8c1f83c56fc7fc5524703a663
mohamed <mteltipr03@gmail.com>
2025-06-10 15:54:31 +0100

    replace 'memset' by 'ft_memset' in the main.c be cause we are not allowed to use memset

--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 21:33:44 by aamraouy          #+#    #+#             */
-/*   Updated: 2025/05/30 11:58:55 by motelti          ###   ########.fr       */
+/*   Updated: 2025/06/10 15:45:02 by mohamed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ int	main(int ac, char **av, char **envp)
 
 	(void)ac;
 	(void)av;
-	memset(&minishell, 0, sizeof(t_shell));
+	ft_memset(&minishell, 0, sizeof(t_shell));
 	minishell.exit_status = 0;
 	minishell.env = build_env_list(&minishell, envp);
 	if (!minishell.env)
